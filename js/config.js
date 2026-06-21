6bad3b32b9bfa0667e93964f4b011d32b400ab29
DKondrasevskij05 <killer2dimon@gmail.com>
2026-06-21 04:02:18 +0300

    зміна

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE = 'http://localhost:8000/api/';
+export const API_BASE = 'https://tourism-backend-f5ve.onrender.com/api/';
 export const TOKEN_KEY = 'access_token';
 export const USER_KEY = 'user_data';
